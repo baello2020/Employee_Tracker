1b3b0de0cbfc3a2dc5bc201afdac76cb2b202a89
Orlando <baello2020@gmail.com>
2020-12-27 18:51:04 -0800

    db updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,7 +10,7 @@ VALUES ("Junior Engineer",50000,3),
 ("Manager",65000,4),
 ("Analyst",55000,1);
 INSERT INTO employees (first_name,last_name,role_id,department_id) 
-VALUES ("Joe","Smith", 1, 3),
-("Lucy","Davis",2,2),
-("Angela","Stevens",3,4),
-("Greg","Portnoy",4,1);
+VALUES ("John","Rockefeller", 1, 3),
+("Cornelius","Vanderbilt",2,2),
+("Andrew","Carnegie",3,4),
+("J.P.","Morgan",4,1);
